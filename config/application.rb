f9860d4cf09724edfefc27ae3c320b163207c749
Roman Huk <romanguk@zoho.com>
2023-08-19 14:44:18 +0300

    user postgres from docker-compose, and load creds from .env

--- config/application.rb
+++ config/application.rb
@@ -8,6 +8,8 @@ require 'rails/all'
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
+Dotenv::Railtie.load # load .env file
+
 module EsAssignment
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
