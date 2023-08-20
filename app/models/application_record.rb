b5479c12f51f2321d99a21b34a2247efb93811d1
Roman Huk <romanguk@zoho.com>
2023-08-20 21:55:21 +0100

    ENV var for batch_size

--- app/models/application_record.rb
+++ app/models/application_record.rb
@@ -1,5 +1,7 @@
 # frozen_string_literal: true
 
 class ApplicationRecord < ActiveRecord::Base
+  DEF_BATCH_SIZE = 1000
+
   primary_abstract_class
 end
--- app/models/prompt.rb
+++ app/models/prompt.rb
@@ -4,7 +4,7 @@ class Prompt < ApplicationRecord
   searchkick word_middle: [:content],
              word_start:  [:content],
              highlight:   [:content],
-             batch_size:  100
+             batch_size:  ENV.fetch('SEARCHKICK_BATCH_SIZE', DEF_BATCH_SIZE).to_i
 
   validates :content, presence: true
 end
