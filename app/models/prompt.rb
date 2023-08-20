0ed1fd79a9d3fdd66912555dd824f72f8041c8cd
Roman Huk <romanguk@zoho.com>
2023-08-20 19:08:03 +0100

    even smaller batch_size

--- app/models/prompt.rb
+++ app/models/prompt.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 class Prompt < ApplicationRecord
-  searchkick word_middle: [:content], highlight: [:content], batch_size: 200
+  searchkick word_middle: [:content], highlight: [:content], batch_size: 100
 
   validates :content, presence: true
 end
